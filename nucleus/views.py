d01da9951120b48df7673d2811fb4dfdd414d87d
Karan Bedi <karanbedi0304@gmail.com>
2017-12-03 02:42:48 +0530

    Nearby incidents function

--- nucleus/views.py
+++ nucleus/views.py
@@ -8,4 +8,7 @@ from .models import *
 # Create your views here.
 class IncidentView(generics.ListCreateAPIView):
   queryset = Incident.objects.all()
-  serializer_class = IncidentSerializer 
+  serializer_class = IncidentSerializer
+
+def get_nearby(lat, lng):
+  return Incident.objects.filter(lat__gte=lat-0.0001, lat__sme=lat+0.0001, lng__gte=lng-0.0001, lng__sme=lng+0.0001)
