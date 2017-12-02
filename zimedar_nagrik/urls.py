1703c6e558ff7c36b6a58f5db1eb704ea4167374
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2017-12-03 04:46:26 +0530

    Fix include

--- zimedar_nagrik/urls.py
+++ zimedar_nagrik/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include
+from django.conf.urls import include
 from django.conf.urls import url
 from django.conf import settings
 import nucleus
