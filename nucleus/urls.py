6e7f3a7fff78de87fcc3459f69c01980f23bd43e
Nikhil Sheoran <nikhilsheoran96@gmail.com>
2017-12-03 04:45:10 +0530

    Fix include in url

--- nucleus/urls.py
+++ nucleus/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url
-from django.urls import include
+from django.conf.urls import include
 from .views import *
 
 urlpatterns = [
